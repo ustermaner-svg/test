c5189b88993fea230fda5b439ec77d13c2a86065
ustermaner-svg <ustermaner@gmail.com>
2026-02-22 15:12:18 +0200

    Update C3.cpp

--- C3.cpp
+++ C3.cpp
@@ -3,9 +3,16 @@ using namespace std;
 void printABs(int);
 int main()
 {
-int num;
-cout << "Enter a number:" << endl;
-cin >> num;
-printABs(num);
-return 0;
+	int num;
+	cout << "Enter a number:" << endl;
+	cin >> num;
+	printABs(num);
+	return 0;
+}
+void printABs(int num ) {
+	if (num > 0) {
+		cout << 'a';
+		printABs(num - 1);
+		cout << 'b';
+	}
 }
